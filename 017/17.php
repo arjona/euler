ac33c28f84314f31f65ea7d0da6dfd62b2db6629
Ariel Arjona <arjona@gmail.com>
2009-10-19 18:24:20 -0500

    counts to 19 now

--- 017/17.php
+++ 017/17.php
@@ -1,9 +1,45 @@
 <?php
 
-$digits = array( '0',
-    'one', 'two', 'three',
-    'four', 'five', 'six',
-    'seven', 'eight', 'nine'
-);
+for ($i = 1; $i < 20; $i++) {
+    $w = number_words($i);
+    echo "$w\n";
+}
 
+
+function number_words($num) {
+    $digits = array( '0',
+        'one', 'two', 'three',
+        'four', 'five', 'six',
+        'seven', 'eight', 'nine'
+    );
+
+    $teens = array('ten',
+        'eleven', 'twelve', 'thirteen',
+        'fourteen', 'fifteen', 'sixteen',
+        'seventeen', 'eighteen', 'nineteen'
+    );
+
+    $dec = array('0', '1',
+        'twenty', 'thirty', 'forty',
+        'fifty', 'sixty', 'seventy',
+        'eighty', 'ninety'
+    );
+
+    if ($num > 0 && $num < 10) {
+        return $digits[$num];
+    }
+
+    if ($num >= 10 && $num < 20) {
+        return $teens[$num - 10];
+    }
+/*
+    $num = (string) $num;
+    $num = strrev($num);
+
+    $words = array();
+    for ($d = 0; $d < strlen($num); $d++) {
+        $words[] = $digits[$num[$d]];
+    }
+    */
+}
 
